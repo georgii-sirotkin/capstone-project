2b48b31b348420b529fb84f176056fd1fd43a018
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-10-09 08:54:46 -0400

    Display currently authenticated user. Add log out button

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link as RouterLink,
 } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { CssBaseline, Link, Button } from '@material-ui/core';
 import Login from './Login';
+import LoadingPage from './LoadingPage';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function logOut() {
+    axios.post('/api/auth/logout', {}, { withCredentials: true });
+    setUser(null);
+  }
+
+  useEffect(() => {
+    axios.get('/api/auth/me', { withCredentials: true })
+      .then(response => {
+        setUser(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <LoadingPage />
+  }
 
   return (
     <Router>
@@ -19,7 +43,24 @@ function App() {
         </Route>
         <Route path='/'>
           <div>
-            {user ? `Logged in as ${user.firstName} ${user.lastName}` : 'Not logged in'}
+            {user ? (
+              <>
+                Logged in as {user.firstName} {user.lastName}<br />
+                <Button color='primary' variant='contained' onClick={logOut}>
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                'Not logged in'
+                <Link
+                  component={RouterLink}
+                  to='login'
+                >
+                  Sign in
+                </Link>
+              </>
+            )}
           </div>
         </Route>
       </Switch>
