6b492214bc1e74a5175cac5398dc40e02728746b
Paul Nohtiev <nogtevpasha97@gmail.com>
2020-10-22 23:46:58 -0400

    Add Photo model and photos table

---
+++ models/Photo.js
@@ -0,0 +1,24 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../database/sequelize');
+
+class Photo extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
+  }
+}
+
+Photo.init({
+  name: DataTypes.STRING
+}, {
+  sequelize,
+  modelName: 'Photo',
+  underscored: true,
+});
+
+module.exports = Photo;
