5e1a7755de126f0dd219ef15014b58c525013f34
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-10-29 19:56:22 -0400

    Include thumbnail photo in hotels endpoint response

--- models/Address.js
+++ models/Address.js
@@ -3,14 +3,6 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../database/sequelize');
 
 class Address extends Model {
-  /**
-   * Helper method for defining associations.
-   * This method is not a part of Sequelize lifecycle.
-   * The `models/index` file will call this method automatically.
-   */
-  static associate(models) {
-    // define association here
-  }
 };
 
 Address.init({
@@ -19,7 +11,7 @@ Address.init({
   city: DataTypes.STRING,
   province: DataTypes.STRING,
   country: DataTypes.STRING,
-  postal_code: DataTypes.STRING
+  postalCode: DataTypes.STRING
 }, {
   sequelize,
   modelName: 'Address',
