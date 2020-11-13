e23829384ef9269a33b1f120ae62b754d8d35a7f
Veerpal Kaur <Veerpalkaur5536@conestogac.on.ca>
2020-11-12 22:19:13 -0500

    Generate more test addresses in different cities

--- database/seeders/20201007170137-addresses.js
+++ database/seeders/20201007170137-addresses.js
@@ -33,6 +33,76 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date()
       },
+      {
+        line1: '30 King Street',
+        line2: null,
+        city: 'Niagara Falls',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'M5B 2E9',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '6755 Fallsview Blvd',
+        line2: null,
+        city: 'Niagara Falls',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'M5V 2X3',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '105 King St E',
+        line2: null,
+        city: 'Kitchener',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'N2G2K8',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '78 Forest Hill Dr.',
+        line2: null,
+        city: 'Kitchener',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'M5B 2E9',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '99 Conestoga College Blvd',
+        line2: null,
+        city: 'Kitchener',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'M5V 2X3',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '4355 King Street E',
+        line2: null,
+        city: 'Kitchener',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'N2G2K8',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        line1: '190 Weber St N',
+        line2: null,
+        city: 'Waterloo',
+        province: 'ON',
+        country: 'Canada',
+        postal_code: 'N2G2K8',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
     ]);
   },
 
