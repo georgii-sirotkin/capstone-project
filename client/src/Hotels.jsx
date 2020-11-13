a113b12a7481ff44401a7ecb115ba0354a709f11
Paul Nohtiev <nogtevpasha97@gmail.com>
2020-11-13 00:31:47 -0500

    Add search input with autocomplete

--- client/src/Hotels.jsx
+++ client/src/Hotels.jsx
@@ -5,8 +5,12 @@ import {
   Box,
   Container,
   Grid,
+  TextField,
+  Paper,
+  Button,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import LoadingContentProgress from './LoadingContentProgress';
 import HotelCard from './HotelCard';
 
@@ -26,16 +30,46 @@ export default function Hotels() {
   const classes = useStyles();
   const [hotels, setHotels] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchString, setSearchString] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const [cities, setCities] = useState([]);
 
-  useEffect(() => {
-    axios.get('/api/hotels')
+  const searchOptions = cities.map(city => city.name);
+
+  function search() {
+    setIsSearching(true);
+
+    axios.get(`/api/hotels?city=${searchString}`)
       .then(response => {
         setHotels(response.data);
-        setIsLoading(false);
+        setIsSearching(false);
       })
       .catch(error => {
-        setIsLoading(false);
+        setIsSearching(false);
       })
+  }
+
+  useEffect(() => {
+    function fetchAllHotels() {
+      axios.get('/api/hotels')
+        .then(response => {
+          setHotels(response.data);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          setIsLoading(false);
+        })
+    }
+
+    function fetchCities() {
+      axios.get('/api/cities')
+        .then(response => {
+          setCities(response.data);
+        });
+    }
+
+    fetchAllHotels();
+    fetchCities();
   }, []);
 
   if (isLoading) {
@@ -59,6 +93,39 @@ export default function Hotels() {
           </Box>
         </Grid>
         <Grid item xs={12} md={9}>
+          <Box mb={3}>
+            <Paper elevation={1}>
+              <Box display='flex' p={2} alignItems='flex-end'>
+                <Autocomplete
+                  disableClearable
+                  options={searchOptions}
+                  value={searchString}
+                  onChange={(event, newValue) => {
+                    setSearchString(newValue);
+                  }}
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      placeholder="Destination"
+                      InputProps={{ ...params.InputProps, type: 'search' }}
+                    />
+                  )}
+                />
+                <Box ml={2}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={search}
+                    disabled={isSearching}
+                    disableElevation
+                  >
+                    Search
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Box>
           {hotels.map(hotel => (
             <HotelCard key={hotel.id} hotel={hotel} />
           ))}
