7ec47a48f4f090e4f3ba6a50cc2d51842429fa29
Veerpal Kaur <Veerpalkaur5536@conestogac.on.ca>
2020-11-18 22:55:05 -0500

    Create cities table

---
+++ database/migrations/20201119035054-create-city.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      code: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      country: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cities');
+  }
+};
