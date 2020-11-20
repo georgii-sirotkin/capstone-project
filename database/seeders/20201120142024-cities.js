7be543bcb426046fc65a57c5ef04b349766b170c
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-11-20 09:33:10 -0500

    Seed cities table

---
+++ database/seeders/20201120142024-cities.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('cities', [
+      {
+        name: 'Paris',
+        code: 'PAR',
+        state: null,
+        country: 'France',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Toronto',
+        code: 'YTO',
+        state: 'Ontario',
+        country: 'Canada',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Niagara Falls',
+        code: 'IAG',
+        state: 'Ontario',
+        country: 'Canada',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Kitchener',
+        code: 'ALO',
+        state: 'Ontario',
+        country: 'Canada',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Waterloo',
+        code: 'WLO',
+        state: 'Ontario',
+        country: 'Canada',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('cities', null, {});
+  }
+};
